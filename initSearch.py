89362f05743d23cdcb2aa968d4763ed511e9a063
brunoaosantos <bruno_scp5@hotmail.com>
2017-11-15 19:54:36 +0000

    change variable names and delete prints

--- initSearch.py
+++ initSearch.py
@@ -1,7 +1,7 @@
 
-entryPoints = []
-sanitization = []
-sensitive = []
+patternEntryPoints = []
+paternSanitization = []
+patternSensitive = []
 
 i = 1
 
@@ -11,19 +11,16 @@ for line in patternFile:
 
 	if i == 2:
 		for j in line.rstrip().split(','):
-			if j[1:] not in entryPoints:
-				entryPoints.append(j[1:])
-		print entryPoints 
+			if j[1:] not in patternEntryPoints:
+				patternEntryPoints.append(j[1:])
 	if i == 3:
 		for j in line.rstrip().split(','):
-			if j not in sanitization:
-				sanitization.append(j)
-		print sanitization
+			if j not in paternSanitization:
+				paternSanitization.append(j)
 	if i == 4:
 		for j in line.rstrip().split(','):
-			if j not in sensitive:
-				sensitive.append(j)
-		print sensitive
+			if j not in patternSensitive:
+				patternSensitive.append(j)
 		i = -1
 	i = i + 1
 
