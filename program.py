9a94739c42188b55e9b453ce84807f2504cf4915
brunoaosantos <bruno_scp5@hotmail.com>
2017-11-16 19:59:53 +0000

    code refactoring

--- program.py
+++ program.py
@@ -11,6 +11,41 @@ sanitization = {}
 sensitive = {}
 query = {}
 
+def assign(i):
+	if i['right']['kind'] == "offsetlookup": #assign -> offsetlookup
+
+			entrypoints[i['left']['name']] = i['right']['what']['name']
+
+	if i['right']['kind'] == "encapsed": #assign -> encapsed
+		query[i['left']['name']] = []
+		
+		for j in i['right']['value']:
+			
+			if j['kind'] == "variable":
+				query[i['left']['name']].append(j['name'])
+
+	if i['right']['kind'] == "bin": #assign -> bin
+		query[i['left']['name']] = []
+		
+		if i['right']['left']['kind'] == "variable":
+			query[i['left']['name']].append(i['right']['left']['name'])
+
+		if i['right']['right']['kind'] == "variable":
+			query[i['left']['name']].append(i['right']['right']['name'])
+
+
+	if i['right']['kind'] == "call": #assign -> call
+		sensitive[i['right']['what']['name']] = []
+
+		for j in i['right']['arguments']:
+
+			if j['kind'] == "variable":
+				sensitive[i['right']['what']['name']].append(j['name'])
+
+	if i['right']['kind'] == "variable": #assign -> variable
+		query[i['left']['name']] = [i['right']['name']]
+
+
 patternFile = open("vulnPatterns.txt", "r")
 
 i = 1
@@ -38,37 +73,8 @@ json_data = json.load(JSONslice)
 for i in json_data['children']:
 
 	if i['kind'] == "assign": #assign
-
-		if i['right']['kind'] == "offsetlookup": #assign -> offsetlookup
-
-			entrypoints[i['left']['name']] = i['right']['what']['name']
-
-		if i['right']['kind'] == "encapsed": #assign -> encapsed
-			query[i['left']['name']] = []
-			
-			for j in i['right']['value']:
-				
-				if j['kind'] == "variable":
-					query[i['left']['name']].append(j['name'])
-
-		if i['right']['kind'] == "bin": #assign -> bin
-			query[i['left']['name']] = []
-			
-			if i['right']['left']['kind'] == "variable":
-				query[i['left']['name']].append(i['right']['left']['name'])
-
-			if i['right']['right']['kind'] == "variable":
-				query[i['left']['name']].append(i['right']['right']['name'])
-
-
-		if i['right']['kind'] == "call": #assign -> call
-			sensitive[i['right']['what']['name']] = []
- 
-			for j in i['right']['arguments']:
-
-				if j['kind'] == "variable":
-					sensitive[i['right']['what']['name']].append(j['name'])
-
+		assign(i)
+		
 	if i['kind'] == "call": #call
 		sensitive[i['what']['name']] = []
 
@@ -83,52 +89,9 @@ for i in json_data['children']:
 	if i['kind'] == "while": #while
 		
 		if i['body']: #while -> body
-			#print "ESTOU NO BODY"
 			for j in i['body']['children']:
-				#print j
-
 				if j['kind'] == "assign": #assign
-
-					if j['right']['kind'] == "offsetlookup": #assign -> offsetlookup
-
-						#entrypoints.append([i['right']['what']['name'], i['left']['name']])
-						entrypoints[j['left']['name']] = j['right']['what']['name']
-
-					if j['right']['kind'] == "encapsed": #assign -> encapsed
-						#query.append([i['left']['name']])
-						query[j['left']['name']] = []
-						
-						for k in j['right']['value']:
-							
-							if k['kind'] == "variable":
-								#query[len(query)-1].append(j['name'])
-								query[j['left']['name']].append(k['name'])
-
-					if j['right']['kind'] == "bin": #assign -> bin
-						#query.append([i['left']['name']])
-						query[j['left']['name']] = []
-						
-						if j['right']['left']['kind'] == "variable":
-							#query[len(query)-1].append(i['right']['left']['name'])
-							query[j['left']['name']].append(j['right']['left']['name'])
-
-						if j['right']['right']['kind'] == "variable":
-							query[j['left']['name']].append(j['right']['right']['name'])
-
-
-					if j['right']['kind'] == "call": #assign -> call
-						#sensitive.append([i['right']['what']['name']])
-						sensitive[j['right']['what']['name']] = []
-			 
-						for k in j['right']['arguments']:
-
-							if k['kind'] == "variable":
-								#sensitive[len(sensitive)-1].append(j['name'])
-								sensitive[j['right']['what']['name']].append(k['name'])
-
-					if j['right']['kind'] == "variable": #assign -> variable
-						query[j['left']['name']] = [j['right']['name']]
-						
+					assign(j)
 
 						
 
